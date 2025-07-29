a4ab8229707a37543446af85f734325d4ad1cc72
lei <thenextlei@gmail.com>
2025-07-28 20:29:11 -0700

    0728 exer algorithm

---
+++ 1newbie/src/ch6/exer/Code04_BinaryTree_PostorderTraversal_1.java
@@ -0,0 +1,54 @@
+package ch6.exer;
+
+/**
+ *         A
+ *       /   \
+ *      B     C
+ *     / \   / \
+ *    D   E F   G
+ *
+ * Preorder → "Root First"
+ *
+ * Inorder → "Root in the Middle"
+ *
+ * Postorder → "Root Last"
+ */
+public class Code04_BinaryTree_PostorderTraversal_1 {
+
+    private static class Node<V> {
+        V v;
+        Node<V> left;
+        Node<V> right;
+        Node(V v) {
+            this.v = v;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node<String> rootNode = constructTree();
+        traverse_Postorder(rootNode,0);
+    }
+
+    private static void traverse_Postorder(Node<String> rootNode,int level) {
+        if (rootNode == null) return;
+        traverse_Postorder(rootNode.left,level+1);
+        traverse_Postorder(rootNode.right,level+1);
+        for (int i = 0; i < level; i++) {
+            System.out.print("\t");
+        }
+        System.out.println(rootNode.v);
+
+    }
+
+    private static Node<String> constructTree() {
+        Node<String> rootNode = new Node<>("A");
+        rootNode.left = new Node<>("B");
+        rootNode.right = new Node<>("C");
+        rootNode.left.left = new Node<>("D");
+        rootNode.left.right = new Node<>("E");
+        rootNode.right.left = new Node<>("F");
+        rootNode.right.right = new Node<>("G");
+        return rootNode;
+    }
+
+}
